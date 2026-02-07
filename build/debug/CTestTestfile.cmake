a05657b5b54422ae29d6c0d417d977d806fdb6d6
= <=>
2026-02-07 20:22:02 +0000

    Initial commit

---
+++ build/debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/zeyadhabib/Repos/template_proj
+# Build directory: /Users/zeyadhabib/Repos/template_proj/build/debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[hello_test]=] "/Users/zeyadhabib/Repos/template_proj/build/debug/hello_test")
+set_tests_properties([=[hello_test]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/zeyadhabib/Repos/template_proj/CMakeLists.txt;34;add_test;/Users/zeyadhabib/Repos/template_proj/CMakeLists.txt;0;")
+subdirs("_deps/json-build")
+subdirs("_deps/googletest-build")
---
+++ tests/tests.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <string>
+
+TEST(MathTest, AddsCorrectly) {
+  EXPECT_EQ(1 + 1, 2);
+}
+
+TEST(StringTest, HelloContainsH) {
+  std::string s = "hello";
+  EXPECT_EQ(s.front(), 'h');
+}
